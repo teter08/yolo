0d0fc900d59357e5d83f00e72a0c220574129c61
Barashkov <ivanbarashkov@yandex.ru>
2024-01-30 11:49:00 +0300

    0.01

--- main.py
+++ main.py
@@ -1,16 +1,92 @@
-# This is a sample Python script.
+import cv2
+import urllib.request
+import numpy as np
+import conf
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Replace the URL with the IP camera's stream URL
+url = conf.url
+cap = cv2.VideoCapture(url)
+whT = 320
+confThreshold = 0.5
+nmsThreshold = 0.3
+classesfile = 'coco.names'
+classNames = []
+with open(classesfile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+# print(classNames)
+
+modelConfig = 'yolov3.cfg'
+modelWeights = 'yolov3.weights'
+net = cv2.dnn.readNetFromDarknet(modelConfig, modelWeights)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def findObject(outputs, im):
+    hT, wT, cT = im.shape
+    bbox = []
+    classIds = []
+    confs = []
+    found_cat = False
+    found_bird = False
+    for output in outputs:
+        for det in output:
+            scores = det[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            if confidence > confThreshold:
+                w, h = int(det[2] * wT), int(det[3] * hT)
+                x, y = int((det[0] * wT) - w / 2), int((det[1] * hT) - h / 2)
+                bbox.append([x, y, w, h])
+                classIds.append(classId)
+                confs.append(float(confidence))
+    # print(len(bbox))
+    indices = cv2.dnn.NMSBoxes(bbox, confs, confThreshold, nmsThreshold)
+    # print(indices)
+
+    for i in indices:
+        # i = i[0]
+        print(classNames[classIds[i]])
+        box = bbox[i]
+        x, y, w, h = box[0], box[1], box[2], box[3]
+        if classNames[classIds[i]] == 'bird':
+            found_bird = True
+        elif classNames[classIds[i]] == 'cat':
+            found_cat = True
+
+        if classNames[classIds[i]] == 'car':
+            cv2.rectangle(im, (x, y), (x + w, y + h), (255, 0, 255), 2)
+            cv2.putText(im, f'{classNames[classIds[i]].upper()} {int(confs[i] * 100)}%', (x, y - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255), 2)
+            print(f'car: {len(indices)}')
+            print(found_bird)
+
+        if classNames[classIds[i]] == 'cat':
+            cv2.rectangle(im, (x, y), (x + w, y + h), (255, 0, 255), 2)
+            cv2.putText(im, f'{classNames[classIds[i]].upper()} {int(confs[i] * 100)}%', (x, y - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255), 2)
+            print(found_cat)
+
+        if found_cat and found_bird:
+            print('alert')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+while True:
+    img_resp = urllib.request.urlopen(url)
+    imgnp = np.array(bytearray(img_resp.read()), dtype=np.uint8)
+    im = cv2.imdecode(imgnp, -1)
+    sucess, img = cap.read()
+    blob = cv2.dnn.blobFromImage(im, 1 / 255, (whT, whT), [0, 0, 0], 1, crop=False)
+    net.setInput(blob)
+    layernames = net.getLayerNames()
+    # print(layernames)
+    # outputNames = [layernames[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    outputIndices = net.getUnconnectedOutLayers()
+    outputNames = [layernames[i - 1] for i in outputIndices]
+    # print(net.getUnconnectedOutLayers())
+    outputs = net.forward(outputNames)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    findObject(outputs, im)
+
+    cv2.imshow('IMage', im)
+    cv2.waitKey(1)
